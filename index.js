7310ee7b186b9f499c163f455d6b18c075358ba2
sincere-abayo <abayosincere11@gmail.com>
2025-09-15 04:54:23 +0200

    Update CORS configuration to restrict access to a specific Netlify domain
    - Changed the allowed origin from a wildcard Netlify domain to 'https://braziconnect.rw/', enhancing security by limiting access to a specific site.

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.use(cors({
     'https://braziconnect.netlify.app',
     'https://braziconnect.rw/'
   ],
+  
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
